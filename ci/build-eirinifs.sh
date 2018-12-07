a588697950627d9b5e0115f33eeef460b012dc2a
Georgi Dankov <gddankov@gmail.com>
2018-12-07 17:22:08 +0200

    Update build-eirinfs script
    [#158524788]

--- ci/build-eirinifs.sh
+++ ci/build-eirinifs.sh
@@ -4,8 +4,41 @@ set -xeuo pipefail
 
 # TODO Do we need this?
 # echo "package main" > buildpackapplifecycle/launcher/package.go
+start-docker() {
+  echo 'DOCKER_OPTS="--data-root /scratch/docker --max-concurrent-downloads 10"' > /etc/default/docker
+  service docker start
+  trap 'service docker stop' EXIT
 
-BASEDIR="$(cd $(dirname $0)/.. && pwd)"
+  local max_retries=20
+  local current_retries=0
+  until docker-running; do
+    echo "Current docker status: $(service docker status)"
+    if [ "$current_retries" -gt "$max_retries" ]; then
+        echo "Failed to start docker daemon after $max_retries retries"
+        exit 1
+    fi
+    ((++current_retries))
+    sleep 0.5
+  done
+  echo "Docker started"
+}
+
+docker-running() {
+  docker stats --no-stream > /dev/null 2>&1
+}
+
+mkdir -p go/src/github.com/suhlig
+export GOPATH=$PWD/go
+export PATH=$PATH:$GOPATH/bin
+
+cp -r eirinifs go/src/github.com/suhlig
+
+start-docker
+
+
+pushd go/src/github.com/suhlig/eirinifs
+BASEDIR="$(pwd)"
+echo "package main" > $BASEDIR/buildpackapplifecycle/launcher/package.go
 
 ( cd $BASEDIR/launchcmd && GOOS=linux go build -a -o $BASEDIR/image/launch )
 ( cd $BASEDIR/buildpackapplifecycle/launcher && GOOS=linux CGO_ENABLED=0 go build -a -installsuffix static -o $BASEDIR/image/launcher )
